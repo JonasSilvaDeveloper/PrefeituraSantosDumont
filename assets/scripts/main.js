f1e28e4d485112d4b0d4dbf0e6e0c684ddb021e2
jonasborgesweb <jonasborgesweb@gmail.com>
2020-06-04 17:04:56 -0300

    Initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,21 @@
+jQuery(function(){
+  structure.init();
+});
+
+var structure = {
+  init: function(){
+    
+    //AQUI VAI O CODIGO JS 
+    console.log('Funcao JavaScript Página Structure');
+  }
+};
+
+// Função JavaScript da Página Home
+var home = {
+  init: function() {
+
+    //AQUI VAI O CODIGO JS 
+    console.log('Funcao JavaScript Página Home');
+    
+  }
+};
