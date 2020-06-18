8385f13df2f538be7db3dc8811592c8b2644735f
jonasborgesweb <jonasborgesweb@gmail.com>
2020-06-18 18:08:45 -0300

    finish Portal de Serviços

--- scripts/main.js
+++ scripts/main.js
@@ -185,3 +185,35 @@ var contato = {
     })
   }
 }
+
+var portal = {
+  init: function(){
+    //Mascara Telefone
+    var behavior = function (val) {
+      return val.replace(/\D/g, '').length === 11 ? '(00) 00000 - 0000' : '(00) 0000 - 00009';
+    },
+    options = {
+      onKeyPress: function (val, e, field, options) {
+          field.mask(behavior.apply({}, arguments), options);
+      }
+    };
+    $('input[type=tel]').mask(behavior, options);
+
+    $('#form-cnpj').mask('00.000.000/0000-00');
+
+    $('#form-fornecedor').submit(function(e){
+      $(this).find('.required').each(function(){
+        if($(this).val().length !== 0){
+          $(this).parent().find('.form__error').css('opacity','0');
+        }else{
+            e.preventDefault();
+          $(this).parent().find('.form__error').css('opacity','1');
+        }
+      })
+
+      $(this).find('.required').focus(function () {
+        $(this).parent().find('.form__error').css('opacity','0');
+      })
+    })
+  }
+}
